b58402de869e667c4312e136763766186a538176
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-24 09:51:12 -0300

    Exercicio01, atualizado

--- src/Exercicio1/Principal.java
+++ src/Exercicio1/Principal.java
@@ -1,5 +1,6 @@
 package Exercicio1;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,6 +8,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
+        DecimalFormat df = new DecimalFormat("#,##0.00");
 
         Conta c1 = new Conta();
 
@@ -17,15 +19,15 @@ public class Principal {
         c1.numeroConta = rd.nextInt(1000, 9999);
 
         System.out.println("Número do correntista: " +c1.numeroConta);
-        System.out.println("Saldo: " + c1.saldo);
+        System.out.println("Saldo: " + df.format(c1.saldo));
 
         c1.depositar(1000) ;
 
-        System.out.println("Saldo: " + c1.saldo);
+        System.out.println("Saldo: " + df.format(c1.saldo));
 
         c1.saque(500);
 
-        System.out.println("Saldo: " + c1.saldo);
+        System.out.println("Saldo: " + df.format(c1.saldo));
 
 
     }
